35d946678dfde5beaaad21842c60338b523012b6
Ed Barberis <ed.barberis@gmail.com>
2022-03-09 14:56:46 -0500

    Upgraded Terraform to 1.1.7; and added AppDynamics required tagging.

--- main.tf
+++ main.tf
@@ -66,6 +66,7 @@ locals {
     DeploymentEnvironment = "NonProd"
     CreatedBy             = data.aws_caller_identity.current.arn
     IntendedPublic        = "True"
+    ContainsPII           = "False"
     Service               = "FSOLab"
     CostCenter            = var.resource_cost_center_tag
   }
--- versions.tf
+++ versions.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = ">= 4.3"
+      version = ">= 4.4"
     }
 
     kubernetes = {
