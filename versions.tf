5280244186c10c9f575e3fbfeb5940eb4b04d076
Ed Barberis <ed.barberis@gmail.com>
2022-04-22 14:27:38 -0400

    Upgraded Terraform to 1.1.9; and provider for AWS to 4.11.

--- versions.tf
+++ versions.tf
@@ -1,11 +1,11 @@
 # Terraform ----------------------------------------------------------------------------------------
 terraform {
-  required_version = ">= 1.1.8"
+  required_version = ">= 1.1.9"
 
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = ">= 4.9"
+      version = ">= 4.11"
     }
 
     kubernetes = {
