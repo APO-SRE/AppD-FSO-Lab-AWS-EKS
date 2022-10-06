bbb3d57030d362a42ff1e9208b76738dead9931c
Ed Barberis <ed.barberis@gmail.com>
2022-10-06 16:50:54 -0400

    Upgraded Terraform to 1.3.2.

--- versions.tf
+++ versions.tf
@@ -1,24 +1,35 @@
 # Terraform ----------------------------------------------------------------------------------------
 terraform {
-  required_version = ">= 1.2.9"
+  required_version = ">= 1.3.2"
 
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = ">= 4.29"
+      source  = "hashicorp/aws"
+      version = ">= 4.33"
     }
 
     kubernetes = {
-      version = "~> 2.13"
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.13"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.7"
+    }
+
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14"
     }
 
     null = {
-      source = "hashicorp/null"
+      source  = "hashicorp/null"
       version = ">= 3.1"
     }
 
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = ">= 3.4"
     }
   }
